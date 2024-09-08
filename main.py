428528cd0a29da621828d054605713e957b75d1d
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2024-09-07 22:26:47 -0500

    circleshape and playes

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,6 +19,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
